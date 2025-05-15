a31796af55b76b41e3445e0eca640d0196260c9d
Joyce-w <hellojoycewong@gmail.com>
2025-05-15 17:47:19 -0500

    Capture and save task

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,12 @@
 console.log("popup.js");
+
+document.getElementById("addTaskBtn").addEventListener("click", myFunction);
+let input = document.getElementById("newTaskInput")
+function myFunction() {
+    //get value
+    console.log(input.value);
+    //add value
+    localStorage.setItem('task1', input.value);
+}
+
+localStorage.setItem('taskStorage'); 
